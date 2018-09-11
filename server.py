4036562c5c83d7ee862682c29cb3f9d7a1367bc7
Victor Saez <victor.fontana.saez@gmail.com>
2018-09-10 21:29:02 -0300

    partial commit

--- server.py
+++ server.py
@@ -4,62 +4,47 @@ from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 
-db_connect = create_engine('sqlite:///chinook.db')
+db_connect = create_engine('sqlite:///enderecodb.db')
 app = Flask(__name__)
 api = Api(app)
 
 
-class Employees(Resource):
+class Enderecos(Resource):
     def get(self):
         conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+        query = conn.execute("select * from endereco") # This line performs query and returns json result
+        return {'Ceps cadastrados': [i[1] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
     
     def post(self):
         conn = db_connect.connect()
         print(request.json)
-        LastName = request.json['LastName']
-        FirstName = request.json['FirstName']
-        Title = request.json['Title']
-        ReportsTo = request.json['ReportsTo']
-        BirthDate = request.json['BirthDate']
-        HireDate = request.json['HireDate']
-        Address = request.json['Address']
-        City = request.json['City']
-        State = request.json['State']
-        Country = request.json['Country']
-        PostalCode = request.json['PostalCode']
-        Phone = request.json['Phone']
-        Fax = request.json['Fax']
-        Email = request.json['Email']
-        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-                             '{13}')".format(LastName,FirstName,Title,
-                             ReportsTo, BirthDate, HireDate, Address,
-                             City, State, Country, PostalCode, Phone, Fax,
-                             Email))
+        id = request.json['id']
+        cep = request.json['cep']
+        logradouro = request.json['logradouro']
+        inicio = request.json['inicio']
+        fim = request.json['fim']
+
+        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}')".format(cep, logradouro, inicio, fim))
         return {'status':'success'}
 
-    
-class Tracks(Resource):
-    def get(self):
+class Enderecos_Id(Resource):
+    def get(self, id):
         conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute("select * from endereco where id ="+id)
+        result = {'Endereco': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
-    
-class Employees_Name(Resource):
-    def get(self, employee_id):
+class Enderecos_Cep(Resource):
+    def get(self, cep):
         conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute("select * from endereco where cep ="+cep)
+        result = {'Endereco': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
 
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+api.add_resource(Enderecos_Cep, '/enderecos/cep/<cep>') # Route_3
+api.add_resource(Enderecos_Id, '/enderecos/id/<id>') # Route_3
+api.add_resource(Enderecos, '/enderecos') # Route_1
 
 
 if __name__ == '__main__':
